cceac0b87efd0cb65b52fccaebc35b3aa214c8ac
jean0518 <sotoortizjeanmichael@gmail.com>
2025-11-19 16:30:48 -0500

    AuthStore (Login)

---
+++ inventarios-sempertex/src/store/AuthStore.jsx
@@ -0,0 +1,12 @@
+import { create } from "zustand"
+export const useAuthStore=create((set, get) => ({
+    signInWithEmail: async (p) => {
+        const {data, error} = await supabase.auth.signInWithPassword({
+            email: p.correo,
+            password: p.password
+        })
+        if(error){
+            return null;
+        }
+    }
+}))
