e542b00ae2715f2ddea8c549522cc79a231ee732
jean0518 <sotoortizjeanmichael@gmail.com>
2025-11-26 16:31:54 -0500

    Update main, to QueryClient for dev

--- inventarios-sempertex/src/main.jsx
+++ inventarios-sempertex/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import {QueryClient, QueryClientProvider } from '@tanstack/react-query'
+const queryClient = new QueryClient()
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <App />
+    </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
