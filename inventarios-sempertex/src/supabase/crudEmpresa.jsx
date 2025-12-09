4ad59b8dafea5c99d05136b479c555ae883e8833
jean0518 <sotoortizjeanmichael@gmail.com>
2025-12-09 16:55:34 -0500

    Add crud empresa for supabase API

---
+++ inventarios-sempertex/src/supabase/crudEmpresa.jsx
@@ -0,0 +1,15 @@
+import Swal from "sweetalert2";
+import {supabase} from "../index"
+
+export const MostrarEmpresa = async(p) => {
+    const {error,data} = await supabase.from("asignarempresa").select(`empresa(id, nombre, simbolomoneda)`).eq("id_usuario",p.idusuario).maybeSingle();
+    if (data){
+        return data;
+    }
+};
+export const ContarUsuariosXempresa = async (p) => {
+    const {data, error} = await supabase.rpc("contar_usuarios_por_empresa",{_id_empresa:p.id_empresa})
+    if (data) {
+        return data;
+    }
+}
