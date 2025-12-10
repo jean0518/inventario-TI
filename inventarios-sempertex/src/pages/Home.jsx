a18de4f116633a655a84eefff5e00de820136942
jean0518 <sotoortizjeanmichael@gmail.com>
2025-12-10 16:16:37 -0500

    Insert contador por empresa

--- inventarios-sempertex/src/pages/Home.jsx
+++ inventarios-sempertex/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 
-import { HomeTemplate } from "../index";
+import { useQuery } from "@tanstack/react-query";
+import { HomeTemplate, useEmpresaStore } from "../index";
 export function Home(){
+    const {contadorusuariosXempresa, dataempresa} = useEmpresaStore(); 
+    const {data} = useQuery(
+        {
+            queryKey:["contar usuarios por empresa", {idempresa:dataempresa.empresa?.id}], 
+            queryFn:()=>contadorusuariosXempresa({id_empresa:dataempresa.empresa?.id}),
+            enabled:!!dataempresa
+        })
     return (<HomeTemplate/>)
 }
