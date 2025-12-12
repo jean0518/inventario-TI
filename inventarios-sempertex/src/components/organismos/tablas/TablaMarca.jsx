61274d3cbbd8b55f4dd384619407ed8c01eeb19e
jean0518 <sotoortizjeanmichael@gmail.com>
2025-12-12 15:51:43 -0500

    insert tabla in page

---
+++ inventarios-sempertex/src/components/organismos/tablas/TablaMarca.jsx
@@ -0,0 +1,24 @@
+import { useReactTable } from "@tanstack/react-table"
+import styled from "styled-components";
+
+export function TablaMarca({data}) {
+    const columns = [
+        {
+            
+        }
+    ]
+    const table = useReactTable({
+        
+    });
+
+    return (
+        <Container>
+            <h1>Componente</h1>
+        </Container>
+
+    );
+}
+
+const Container = styled.div`
+
+`
