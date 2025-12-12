db12de8f982fd24f211c844a159e547a95651f5d
jean0518 <sotoortizjeanmichael@gmail.com>
2025-12-12 15:49:08 -0500

    Function marca with sabe data

--- inventarios-sempertex/src/pages/Marca.jsx
+++ inventarios-sempertex/src/pages/Marca.jsx
@@ -1,4 +1,23 @@
-import { MarcaTemplate } from "../index";
+import { useQuery } from "@tanstack/react-query";
+import { MarcaTemplate, SpinnerLoader, useEmpresaStore, useMarcaStore } from "../index";
+
 export function Marca(){
-    return (<MarcaTemplate/>);
+    const {mostrarMarca, datamarca, buscarMarca, buscador} = useMarcaStore();
+    const {dataempresa} = useEmpresaStore();
+    const {isLoading,error} = useQuery({
+        queryKey:["mostrar marca", {id_empresa:dataempresa.id}],
+        queryFn:()=>mostrarMarca({id_empresa:dataempresa.id}),enabled:dataempresa.id!=null
+    });
+
+    const {data:buscardata} = useQuery({
+        queryKey:["buscar marca", {id_empresa:dataempresa.id}],
+        queryFn:()=>buscarMarca({id_empresa:dataempresa.id,descripcion:buscador}),enabled:dataempresa.id!=null
+    });
+    if (isLoading){
+        return <SpinnerLoader/>
+    }
+    if(error){
+        return <span>Error...</span>;
+    }
+    return (<MarcaTemplate data={datamarca}/>);
 }
