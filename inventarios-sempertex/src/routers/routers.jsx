d9260b93b57883e6740c7ed02c5ea235d6d6bdac
jean0518 <sotoortizjeanmichael@gmail.com>
2026-03-24 17:16:19 -0500

    Add data permisos

--- inventarios-sempertex/src/routers/routers.jsx
+++ inventarios-sempertex/src/routers/routers.jsx
@@ -1,21 +1,27 @@
 import {Routes, Route} from "react-router-dom"
-import {Configuracion, ErrorMolecula, Home, Login, Marca, ProtectedRoute, SpinnerLoader, useEmpresaStore, UserAuth, useUsuariosStore, Categorias, Productos, Personal} from "../index"
+import {Configuracion, ErrorMolecula, Home, Login, Marca, ProtectedRoute, SpinnerLoader, useEmpresaStore, UserAuth, useUsuariosStore, Categorias, Productos, Usuarios} from "../index"
 import { useQuery } from "@tanstack/react-query"
 export function MyRoutes() {
     const {user} = UserAuth()
-    const {mostrarUsuarios, idusuario} = useUsuariosStore()
+    const {mostrarUsuarios, idusuario, mostrarpermisios} = useUsuariosStore()
     const {mostrarEmpresa} = useEmpresaStore()
     const {data:datausuarios,isLoading,error} = useQuery(
         {
             queryKey:["Mostrar usuarios"],
             queryFn: ()=>mostrarUsuarios(),
         });
-        const {data:dataempresa}=useQuery(
-            {
-                queryKey:["mostrar empresa"],
-                queryFn:() => mostrarEmpresa({idusuario:idusuario}),
-                enabled:!!datausuarios
-            })
+    const {data:dataempresa}=useQuery(
+        {
+            queryKey:["mostrar empresa"],
+            queryFn:() => mostrarEmpresa({idusuario:idusuario}),
+            enabled:!!datausuarios
+        })
+    const {data:datapermisos}=useQuery(
+        {
+            queryKey:["mostrar permisos", {id_usuario:idusuario}],
+            queryFn:() => mostrarpermisios({id_usuario:idusuario}),
+            enabled:!!datausuarios
+        })
         if(isLoading){ 
             return <SpinnerLoader/>
         }
@@ -31,7 +37,7 @@ export function MyRoutes() {
                 <Route path="/configurar/marca" element={<Marca />} />
                 <Route path="/configurar/categorias" element={<Categorias />} />
                 <Route path="/configurar/productos" element={<Productos />} />
-                <Route path="/configurar/personal" element={<Personal />} />
+                <Route path="/configurar/personal" element={<Usuarios />} />
             </Route>
         </Routes>
 
