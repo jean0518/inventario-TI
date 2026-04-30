b3417d01d6d76f4fa801a5a4595262b6872edb54
jean0518 <sotoortizjeanmichael@gmail.com>
2026-04-29 22:40:27 -0500

    Main file updated

--- inventarios-sempertex/src/main.jsx
+++ inventarios-sempertex/src/main.jsx
@@ -5,7 +5,14 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import {QueryClient, QueryClientProvider } from '@tanstack/react-query'
-const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  },
+});
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
